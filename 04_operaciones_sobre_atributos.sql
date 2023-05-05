7d943c46c93be10c54f18b54b094d74d921ccea0
unknown <yesid9523@gmail.com>
2023-05-04 20:10:15 -0500

    Script de llaves candidatas creado

--- 04_operaciones_sobre_atributos.sql
+++ 04_operaciones_sobre_atributos.sql
@@ -2,11 +2,20 @@
 -- Agregar una columna
 ALTER TABLE usuarios ADD COLUMN nacimiento VARCHAR(15);
 
+-- Agregacion total
+ALTER TABLE users ADD id INT UNSIGNED NOT NULL DEFAULT 'Sin apellido' AUTO_INCREMENT;
+
 -- Modificar EL TIPO DE DATO de una columna
 ALTER TABLE usuarios MODIFY nacimiento DATE;
 
 -- Modificar total
-ALTER TABLE usuarios MODIFY correo bigint(20) unsigned NOT NULL default '0';
+ALTER TABLE usuarios MODIFY correo bigint(20) UNSIGNED NOT NULL default '0';
+
+-- Eliminar modificador NOT NULL
+ALTER TABLE usuarios MODIFY apellido VARCHAR(30) NOT NULL;
+
+-- Agregar modificador NOT NULL
+ALTER TABLE usuarios MODIFY apellido VARCHAR(30) NULL;
 
 -- Renombrar 
 ALTER TABLE usuarios CHANGE nacimiento fechanacimiento VARCHAR(15);
@@ -15,5 +24,3 @@ ALTER TABLE usuarios CHANGE nacimiento fechanacimiento VARCHAR(15);
 -- Eliminar
 ALTER TABLE usuarios DROP COLUMN fecha_nacimiento;
 
--- Agregacion total
-ALTER TABLE users ADD id INT UNSIGNED NOT NULL AUTO_INCREMENT;
