f1a9b492be6c23c6ce57e317561c30490abb8adb
unknown <yesid9523@gmail.com>
2023-05-16 21:50:14 -0500

    Vaciar tabla en el DELETE FROM

--- 20_eliminacion_(DELETE).sql
+++ 20_eliminacion_(DELETE).sql
@@ -3,4 +3,6 @@
 -- Importante: DELETE siempre debe ir con la clausULA WHERE
 DELETE FROM usuarios WHERE usuario_id = 12;
 
+-- Vaciar tabla con REFERENCIAS
+DELETE FROM nombre_tabla WHERE cualquier_atributo LIKE '%';
 -- Ver mas: https://dev.mysql.com/doc/refman/5.7/en/delete.html
